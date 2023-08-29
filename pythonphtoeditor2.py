131d293f67dc01f452d3058ef04a9b78260ed479
Rotimi Awomodu <rotimiawomodu@gmail.com>
2023-08-29 23:09:25 +0100

    2nd first project commit

--- pythonphtoeditor2.py
+++ pythonphtoeditor2.py
@@ -1,17 +1,17 @@
 from PIL import Image, ImageEnhance, ImageFilter
 import os 
 
-path = './samples'
-pathOut = '/editedsample'
+path = './Python-Projects/samples'
+pathOut = '/Python-Projects/editedsample2'
 
 for file_name in os.listdir(path):
     img = Image.open('{}/{}'.format(path ,file_name))
     
     edit = img.filter(ImageFilter.SHARPEN).convert('L')
     
-    # factor = 1.5
-    # enhancer =
-    
+    factor = 1.5
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(factor)
     clean_name = os.path.splitext(file_name)[0]
     
     edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
